492bfe2289058ec0dd6c71d2281aae4283c13c1d
dquishpe <dquishpe@espol.edu.ec>
2024-12-08 18:51:36 -0500

    Uso de useState, useRef y useEfffect

--- src/components/TableWeather.tsx
+++ src/components/TableWeather.tsx
@@ -5,51 +5,50 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { useState, useEffect } from 'react';
+import Item from '../interfaces/Item';
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
+interface MyProp {
+  itemsIn: Item[];
 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+export default function BasicTable(props: MyProp) {
+  const [rows, setRows] = useState<Item[]>([]);
 
-export default function BasicTable() {
+  useEffect(() => {
+    setRows(props.itemsIn);
+  }, [props.itemsIn]);
+
+  // Función para extraer la hora de una fecha en formato ISO
+  const extractTime = (isoString: String): String => {
+    return isoString.split('T')[1]?.split('+')[0]; // Toma la hora y elimina la zona horaria, si existe
+  };
+
   return (
     <TableContainer component={Paper}>
-      <Table aria-label="simple table">
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Hora de inicio</TableCell>
+            <TableCell align="right">Hora de fin</TableCell>
+            <TableCell align="right">Precipitación</TableCell>
+            <TableCell align="right">Humedad</TableCell>
+            <TableCell align="right">Nubosidad</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {rows.map((row, idx) => (
             <TableRow
-              key={row.name}
+              key={idx}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {extractTime(row.dateStart)}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{extractTime(row.dateEnd)}</TableCell>
+              <TableCell align="right">{row.precipitation}</TableCell>
+              <TableCell align="right">{row.humidity}</TableCell>
+              <TableCell align="right">{row.clouds}</TableCell>
             </TableRow>
           ))}
         </TableBody>
